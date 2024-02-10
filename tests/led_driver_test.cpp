aa27a2c500296f8a8b8730836d29cc7fb255677b
Chel_Mikhail <chirlushkin@mail.ru>
2024-02-10 19:11:44 +0300

    Refactoring of 3 first tests.

--- tests/led_driver_test.cpp
+++ tests/led_driver_test.cpp
@@ -8,8 +8,11 @@ extern "C"
 
 TEST_GROUP(LedDriver)
 {
+	// If var virtualLeds is defined static, the solution has problems with linking.
+	uint16_t virtualLeds;
 	void setup()
 	{
+		led_driver_create(&virtualLeds);
 	}
 
 	void teardown()
@@ -26,16 +29,12 @@ TEST(LedDriver, LedsOffAfterCreate)
 
 TEST(LedDriver, TurnOnLedOn)
 {
-	uint16_t virtualLeds;
-	led_driver_create(&virtualLeds);
 	led_driver_turn_on(1);
 	BITS_EQUAL(1, virtualLeds, 0xffff);
 }
 
 TEST(LedDriver, TurnOffLedOne)
 {
-	uint16_t virtualLeds;
-	led_driver_create(&virtualLeds);
 	led_driver_turn_on(1);
 	led_driver_turn_off(1);
 	BITS_EQUAL(0, virtualLeds, 0xffff);
